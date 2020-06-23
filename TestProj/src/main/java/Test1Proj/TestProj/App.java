514d26b7edbdd1ffcf43a649c4090754117b2b71
Shashank Kumar Srivastava <shashank.dir@gmail.com>
2020-06-23 22:44:09 +0530

    Update BY Anshu
    Add Second Line of SOP

--- TestProj/src/main/java/Test1Proj/TestProj/App.java
+++ TestProj/src/main/java/Test1Proj/TestProj/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!"+" "+"This is shashank" );
+        System.out.println( "Hello World!"+"This is shashank" );
     }
 }
